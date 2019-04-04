a66bcb9bc7f93cb8e6ba5d28f125872d4a90c517
Bruno Martins <sgtbrunoalmeida@gmail.com>
2019-04-03 23:40:05 -0300

    Versão 0.2

--- Double_Binary_Search_2.py
+++ Double_Binary_Search_2.py
@@ -2,29 +2,19 @@ import time
 '''
 Verifica se um conjunto possui mais elementos que o outro subset(D) > subset(Q)
 '''
-def ComparaConjuntos(D,Q,inicioD, fimD, inicioQ, fimQ):
-    print("Tamanho Vetor D:",len(D)," e Tamanho Vetor Q:",len(Q))
-    #print("Indices: ", inicioD, fimD, inicioQ, fimQ)
+def comparaConjuntos(D,Q,inicioD, fimD, inicioQ, fimQ):
     result = True
     vet_aux1 = []
     vet_aux2 = []
-    if inicioD > fimD:
-        aux = inicioD
-        inicioD = fimD
-        fimD = aux
-    if inicioQ > fimQ:
-        aux = inicioQ
-        inicioQ = fimQ
-        fimQ = aux
     for i in range(inicioD, fimD):
         vet_aux1.append(D[i])
     for i in range(inicioQ, fimQ):
-        vet_aux2.append(Q[i])
+        vet_aux1.append(Q[i])
     if len(vet_aux1) > len(vet_aux2):
         result = True
     else:
         result = False
-    return [result, vet_aux1, vet_aux2]
+    return result
 
 '''
 Funcao de busca binaria
@@ -40,10 +30,7 @@ def BinSearch(chave, D, minD, maxD): #Retorna posicao
         else:
             return [q, chave] #Retorna a posicao e valor ## Achou o valor midQval
     #Elemento nao encontrado no vetor
-    if maxD+1 < len(D):
-        return [maxD+1, -1] #Retorna a posicao mais alta depois do valor a ser encontrado \  ex: 5 entre 4 e 6; retorna a posicao do 6
-    else:
-        return [maxD, -1]
+    return [minD-1, -1] #Retorna a posicao mais alta depois do valor a ser encontrado \  ex: 5 entre 4 e 6; retorna a posicao do 6
 
 
 '''
@@ -56,7 +43,7 @@ def Intersect(D, Q, minD, maxD, minQ, maxQ, result):
     if minD > maxD or minQ > maxQ:
         print("Para recursao")
         return
-    midQ = (minQ+maxQ) // 2 #Pega a posicao do elemento do meio
+    midQ = round((minQ+maxQ) / 2) #Pega a posicao do elemento do meio
     midQval = Q[midQ] #Pega o valor do elemento do meio
     busca_binaria = BinSearch(midQval, D, minD, maxD) # Faz a busca binaria do elemento de Q no vetor D
     midD = busca_binaria[0] #Recebe a posicao do valor no Vetor D ou a proxima posicao se o elemento nao for encontrado
@@ -68,16 +55,20 @@ def Intersect(D, Q, minD, maxD, minQ, maxQ, result):
     Caso nao seja, a logica da procura eh invertida
     '''
     print("")
-    result_comparacao = ComparaConjuntos(D,Q,minD, midD-1, minQ, midQ-1)
-    #result_comparacao = ComparaConjuntos(D,Q,minD, midD, minQ, midQ)
-    print('1 - %s > %s' % (str(result_comparacao[1]),str(result_comparacao[2])))
-    if result_comparacao[0]:
+    #print('1 - D[midD -1]:%d > Q[minQ]:%d ' % (D[midD - 1], Q[minQ]))
+    #print('1 - D[midD -1]:%d > Q[minQ]:%d ' % (D[midD], Q[minQ]))
+    #if D[midD - 1] > Q[midQ - 1]:
+    #if D[midD-1] > Q[minQ]:
+    #print("Resultado: ",comparaConjuntos(D,Q,minD, midD-1, minQ, midQ-1))
+    #if D[midD-1] > Q[minQ]:
+    #if comparaConjuntos(D,Q,minD, midD-1, minQ, midQ-1):
+    if comparaConjuntos(D,Q,minD, midD, minQ, midQ):
         print('1.1 - D, Q, %d, %d, %d, %d' %( minD,midD - 1, minQ, midQ - 1))
-        Intersect(D, Q, minD,midD - 1, minQ, midQ - 1, result)
+        print(Intersect(D, Q, minD,midD - 1, minQ, midQ - 1, result))
         print("")
     else:
         print('1.2 - Q, D, %d, %d, %d, %d' % (minQ, midQ - 1, minD, midD - 1))
-        Intersect(Q, D, minQ, midQ - 1, minD, midD - 1, result)
+        print(Intersect(Q, D, minQ, midQ - 1, minD, midD - 1, result))
         print("")
 
     '''
@@ -85,7 +76,7 @@ def Intersect(D, Q, minD, maxD, minQ, maxQ, result):
     Se sim, entao adiciona o valor a resposta, pois corresponde a uma interseccao 
     '''
     print("")
-    print('2 - D[midD]:%d == midQval:%d ' % (D[midD], midQval))
+    #print('2 - D[midD]:%d == midQval:%d ' % (D[midD], midQval))
     if D[midD] == midQval:
         print("2.1 - Adicionando o resultado no vetor")
         result.append(midQval)
@@ -98,17 +89,18 @@ def Intersect(D, Q, minD, maxD, minQ, maxQ, result):
     Caso nao seja, a logica da procura eh invertida
     '''
     print("")
-    result_comparacao = ComparaConjuntos(D, Q, midD+1,maxD,midQ+1,maxQ)
-    #result_comparacao = ComparaConjuntos(D, Q, midD,maxD,midQ,maxQ)
-    print('3 - %s > %s' % (str(result_comparacao[1]), str(result_comparacao[2])))
-    if result_comparacao[0]:
+    #print('3 - D[maxD]:%d > Q[midQ]:%d ' % (D[maxD], Q[midQ]))
+    #print(comparaConjuntos(D, Q, midD+1,maxD,midQ+1,maxQ))
+    #if D[maxD] > Q[midQ]:
+    #if comparaConjuntos(D, Q, midD+1,maxD,midQ+1,maxQ):
+    if comparaConjuntos(D, Q, midD,maxD,midQ,maxQ):
     #if D[maxD] > Q[maxQ]:
         print('3.1 - D, Q, %d, %d, %d, %d' % (midD, maxD, midQ + 1, maxQ))
-        Intersect(D, Q, midD, maxD, midQ + 1, maxQ, result)
+        print(Intersect(D, Q, midD, maxD, midQ + 1, maxQ, result))
         print("")
     else:
         print('3.2 - Q, D, %d, %d, %d, %d' % (midQ + 1, maxQ, midD, maxD))
-        Intersect(Q, D, midQ + 1, maxQ, midD, maxD, result)
+        print(Intersect(Q, D, midQ + 1, maxQ, midD, maxD, result))
         print("")
 
 
@@ -132,18 +124,16 @@ print(posicao)
 '''
 
 
-#D = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 30, 40, 50, 60, 70, 80, 90, 100]
-#D = [2,10, 17, 30, 70, 90, 100, 101, 102,103,107,110,230,235,1000]
-#Q = [1,2,3,4,5,10]
+D = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 30, 40, 50, 60, 70, 80, 90, 100]
+#Q = [0, 1, 2, 3, 4, 5, 17, 30, 70, 90, 100]
+Q = [20, 60, 101]
 #Q = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 30, 40, 50, 60, 70, 80, 90, 100]
 #D = [2, 4, 6, 8, 10]
 #Q = [1, 2, 3, 4]
 #D = [0, 1, 2, 3, 4, 5]
 #Q = [0, 3, 5, 6, 7, 8]
 #D = [0, 1, 2, 3]
-#Q = [0,3]
-D = list(range(0,2000))
-Q = [0, 100, 101, 234, 2001]
+#Q = [3]
 result = []
 result_intersect = (Intersect(D, Q, 0, len(D)-1, 0, len(Q)-1, result))
 
